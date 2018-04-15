c29aee4b49b4f9d5d59e51a07300f1e5e6291840
glebka777 <glebdo777@gmail.com>
2018-04-15 20:53:21 +0300

    untry express xd

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 const socks = require("@heroku/socksv5");
 const socketIO = require("socket.io");
-const express = require("express");
 
 const port = process.env.PORT || 1080;
 const users = JSON.parse(process.env.USERS) || {};
 
 
-// const server = socks.createServer(function(info, accept, deny) {
-//   accept();
-// }).on('error', (err) => {
-//   console.log('Got error: ' + err);
-// }).on('connection', (connection) => {
-//   console.log('Connection: ' + connection.srcAddr + ':' + connection.srcPort + ' => ' + connection.dstAddr + ':' + connection.dstPort);
-// });
-// server.useAuth(socks.auth.UserPassword(function(user, password, cb) {
-//   let foundPassword = users[user];
-//   let matched = (password === foundPassword);
-//   if (matched)
-//     console.log('Authorized user: ' + user);
-//   else
-//     console.log('Failed login attempt. User: ' + user);
-//   cb(matched);
-// }));
-// server.listen(port, function() {
-//   console.log('SOCKS server listening on port ' + port);
-// });
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(port, () => console.log(`Listening on ${port}`));
+const server = socks.createServer(function(info, accept, deny) {
+  accept();
+}).on('error', (err) => {
+  console.log('Got error: ' + err);
+}).on('connection', (connection) => {
+  console.log('Connection: ' + connection.srcAddr + ':' + connection.srcPort + ' => ' + connection.dstAddr + ':' + connection.dstPort);
+});
+server.useAuth(socks.auth.UserPassword(function(user, password, cb) {
+  let foundPassword = users[user];
+  let matched = (password === foundPassword);
+  if (matched)
+    console.log('Authorized user: ' + user);
+  else
+    console.log('Failed login attempt. User: ' + user);
+  cb(matched);
+}));
+server.listen(port, function() {
+  console.log('SOCKS server listening on port ' + port);
+});
 
 const io = socketIO.listen(server);
 io.on('connection', (socket) => {
