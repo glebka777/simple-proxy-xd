8414aa84e9a926202ae05675153eefddf30243f6
glebka777 <glebdo777@gmail.com>
2018-04-15 15:11:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const socks = require("@heroku/socksv5");
+
+const port = process.env.PORT || 1080;
+const users = JSON.parse(process.env.USERS) || {};
+
+
+const srv = socks.createServer(function(info, accept, deny) {
+  accept();
+}).on('error', (err) => {
+  console.log('Got error: ' + err);
+}).on('connection', (connection) => {
+  console.log('Connection: ' + connection.srcAddr + ':' + connection.srcPort + ' => ' + connection.dstAddr + ':' + connection.dstPort);
+});
+
+srv.useAuth(socks.auth.UserPassword(function(user, password, cb) {
+  let foundPassword = users[user];
+  let matched = (password === foundPassword);
+  if (matched)
+    console.log('Authorized user: ' + user);
+  else
+    console.log('Failed login attempt. User: ' + user);
+  cb(matched);
+}));
+
+srv.listen(port, function() {
+  console.log('SOCKS server listening on port ' + port);
+});
