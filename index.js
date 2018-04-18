a087a094f680244c3f7620fa84de92392c3ba48a
glebka777 <glebdo777@gmail.com>
2018-04-19 00:42:56 +0300

    remove web-sockets

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const socks = require("@heroku/socksv5");
-const WebSocketServer = require("ws").Server;
-const express = require("express");
 
 const port = process.env.PORT || 1080;
 const users = JSON.parse(process.env.USERS) || {};
 
-
-const app = express();
-app.use(express.static(__dirname + "/"));
 
 const server = socks.createServer(function(info, accept, deny) {
   accept();
@@ -27,12 +22,4 @@ server.useAuth(socks.auth.UserPassword(function(user, password, cb) {
 }));
 server.listen(port, function() {
   console.log('SOCKS server listening on port ' + port);
-});
-
-
-const wss = new WebSocketServer({server: server});
-
-wss.on('connection', (socket) => {
-  console.log('Client connected via web-socket');
-  socket.on('disconnect', () => console.log('Client disconnected via web-socket'));
 });
